552a2b86fcbad125c7a903090a2911de0897f3b6
Max Melhuish <maxmelhuish@gmail.com>
2023-10-07 20:57:25 +1100

    started if logic for save track butotn

--- app/assets/scripts/saveTrack.js
+++ app/assets/scripts/saveTrack.js
@@ -2,8 +2,12 @@
       $("button").click(function(){
         var $button = $( this )
         let buttonValue = $button.attr("value")
+        let buttonId = $button.attr("id")
         console.log(buttonValue);
-        $.post(`/save?trackId=${buttonValue}`,
+        console.log(buttonId);
+
+        //TODO finish testing this if logic - only post if save-track button is pressed
+        if(buttonId === "save-track") {$.post(`/save?trackId=${buttonValue}`} ,
         {},
         function(data,status){
           alert("Track saved to your library");
